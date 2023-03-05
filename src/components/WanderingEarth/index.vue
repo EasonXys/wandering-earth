8e5ccf92948c2e60a4e5ea3737eb9b43ad791ef1
XuyuSong <43518332+EasonXys@users.noreply.github.com>
2023-03-05 20:23:49 +0800

    feat: 插入blender模型

--- src/components/WanderingEarth/index.vue
+++ src/components/WanderingEarth/index.vue
@@ -7,6 +7,7 @@ import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { engineBaseInfo, IEngineInfo } from '../../constants'
 import { getEngineBody } from '../../utils/Engine'
 import { generateStars } from '../../utils/Stars'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 
 import earthImg from '@/assets/earth_new.jpeg'
 import cloudsImg from '@/assets/earth/earth_cloud.jpeg'
@@ -20,7 +21,8 @@ import skyImgs from './img'
 let time = 0;
 let flameMats: any = [];
 const clock = new THREE.Clock();
-
+let objLoader;
+let atmosphere;
 onMounted(() => {
 
   // 场景
@@ -35,39 +37,39 @@ onMounted(() => {
     backSkyImg,
   } = skyImgs
 
-  // 天空盒子
-  let urls = [
-    leftSkyImg,
-    rightSkyImg,
-    topSkyImg,
-    bottomSkyImg,
-    frontSkyImg,
-    backSkyImg,
+  // // 天空盒子
+  // let urls = [
+  //   leftSkyImg,
+  //   rightSkyImg,
+  //   topSkyImg,
+  //   bottomSkyImg,
+  //   frontSkyImg,
+  //   backSkyImg,
 
-  ];
-  let skyboxCubemap = new THREE.CubeTextureLoader().load(urls)
-  skyboxCubemap.format = THREE.RGBAFormat;
-  let skyboxShader = THREE.ShaderLib['cube'];
-  skyboxShader.uniforms['tCube'].value = skyboxCubemap;
-  let skyBox = new THREE.Mesh(
-    new THREE.BoxGeometry(10000, 20000, 20000),
-    new THREE.ShaderMaterial({
-      fragmentShader: skyboxShader.fragmentShader,//片段着色器
-      vertexShader: skyboxShader.vertexShader,//顶点着色器
-      uniforms: skyboxShader.uniforms,//是所有顶点都具有相同的值的变量。 比如灯光，
-      // 雾，和阴影贴图就是被储存在uniforms中的数据。 uniforms可以通过顶点着色器和片元着色器来访问。
-      depthWrite: false,//深度测试
-      side: THREE.BackSide//正反面
-    })
-  );
-  scene.add(skyBox);
-  scene.rotation.y = THREE.MathUtils.degToRad(150);
+  // ];
+  // let skyboxCubemap = new THREE.CubeTextureLoader().load(urls)
+  // skyboxCubemap.format = THREE.RGBAFormat;
+  // let skyboxShader = THREE.ShaderLib['cube'];
+  // skyboxShader.uniforms['tCube'].value = skyboxCubemap;
+  // let skyBox = new THREE.Mesh(
+  //   new THREE.BoxGeometry(10000, 20000, 20000),
+  //   new THREE.ShaderMaterial({
+  //     fragmentShader: skyboxShader.fragmentShader,//片段着色器
+  //     vertexShader: skyboxShader.vertexShader,//顶点着色器
+  //     uniforms: skyboxShader.uniforms,//是所有顶点都具有相同的值的变量。 比如灯光，
+  //     // 雾，和阴影贴图就是被储存在uniforms中的数据。 uniforms可以通过顶点着色器和片元着色器来访问。
+  //     depthWrite: false,//深度测试
+  //     side: THREE.BackSide//正反面
+  //   })
+  // );
+  // scene.add(skyBox);
+  // scene.rotation.y = THREE.MathUtils.degToRad(150);
 
   // 灯光
-  const light = new THREE.AmbientLight(0xeeeeee); // soft white light
+  const light = new THREE.AmbientLight(0xeeeeee, 0.1); // soft white light
   const point_light = new THREE.PointLight(0xffffff, 1.5, 4000, 2);
   point_light.position.set(1000, 0, 0);
-  // scene.add(light);
+  scene.add(light);
   scene.add(point_light);
 
 
@@ -78,10 +80,10 @@ onMounted(() => {
    * @param3  近截面
    * @param4  远截面
    */
-  // const camera = new THREE.OrthographicCamera(-600, 600, 150, -150, 1, 1000);
+  // const camera = new THREE.OrthographicCamera(-800, 800, 400, -400, 1, 1000);
   const camera = new THREE.PerspectiveCamera(80, 2, 1, 2000);
-  camera.position.z = 150;
-  camera.position.x = 100;
+  camera.position.x = 50;
+  camera.position.z = 200;
   camera.position.y = 90;
 
   // 渲染器
@@ -197,7 +199,7 @@ onMounted(() => {
     }
     let atmosphereMaterialOptions = Object.assign(atmosphereMaterialDefaults, options.atmosphere.material);
     let atmosphereMaterial = planetProto.material(atmosphereMaterialOptions);
-    let atmosphere = new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
+    atmosphere = new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
 
     // Create the planet's Atmospheric glow
     let atmosphericGlowGeometry = planetProto.sphere(options.surface.size + options.atmosphere.size + options.atmosphere.glow.size);
@@ -234,54 +236,79 @@ onMounted(() => {
     return planet;
   };
 
-  const earth_sphere = createPlanet({
-    surface: {
-      size: 36,
-      material: {
-        bumpScale: 0.05,
-        specular: new THREE.Color('grey'),
-        shininess: 10
-      },
-      textures: {
-        map: earthImg,
-        bumpMap: bumpMapImg,
-        specularMap: specularMapImg
-      }
-    },
-    atmosphere: {
-      size: 0.3,
-      material: {
-        opacity: 0.8,
-      },
-      textures: {
-        map: cloudsImg
-      },
-      glow: {
-        size: 0.5,
-        intensity: 0.7,
-        fade: 7,
-        color: 0x93cfef
-      }
-    },
-  });
+  // const earth_sphere = createPlanet({
+  //   surface: {
+  //     size: 36,
+  //     material: {
+  //       bumpScale: 0.05,
+  //       specular: new THREE.Color('grey'),
+  //       shininess: 10
+  //     },
+  //     textures: {
+  //       map: earthImg,
+  //       bumpMap: bumpMapImg,
+  //       specularMap: specularMapImg
+  //     }
+  //   },
+  //   atmosphere: {
+  //     size: 0.3,
+  //     material: {
+  //       opacity: 0.8,
+  //       shininess: 10
+  //     },
+  //     textures: {
+  //       map: cloudsImg
+  //     },
+  //     glow: {
+  //       size: 0.5,
+  //       intensity: 0.7,
+  //       fade: 7,
+  //       color: 0x93cfef
+  //     }
+  //   },
+  // });
 
   // 创建流浪地球group
   const earth_group = new THREE.Group();
 
-  earth_group.add(earth_sphere);
+  // earth_group.add(earth_sphere);
 
-  engineBaseInfo.forEach((ei: IEngineInfo) => {
-    const { engine_group, flameMaterials } = getEngineBody(ei, flameMats)
-    flameMats = flameMaterials
-    earth_group.add(engine_group);
+  // engineBaseInfo.forEach((ei: IEngineInfo) => {
+  //   const { engine_group, flameMaterials } = getEngineBody(ei, flameMats)
+  //   flameMats = flameMaterials
+  //   earth_group.add(engine_group);
 
-  })
+  // })
 
 
 
   earth_group.rotation.z = THREE.MathUtils.degToRad(90);
 
+  objLoader = new OBJLoader()
+  objLoader.load(
+    'src/models/engine/engine.obj',
+    (object) => {
+      // (object.children[0] as THREE.Mesh).material = material
+      // object.traverse(function (child) {
+      //     if ((child as THREE.Mesh).isMesh) {
+      //         (child as THREE.Mesh).material = material
+      //     }
+      // })
+      object.position.y = - 50;
+      object.position.x = 50;
+      object.scale.x = 10
+      object.scale.y = 10
+      object.scale.z = 10
 
+      scene.add(object)
+    },
+    (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+      console.log(error)
+    }
+  )
 
   scene.add(earth_group);
 
@@ -306,15 +333,20 @@ onMounted(() => {
     flameMats.forEach((fm: any) => {
       fm.uniforms.time.value = +(time * 20).toFixed(2);
     })
-    // stars_group.position.x -= 0.1
-    // if (stars_group.position.x < -500) {
-    //   stars_group.position.x = 500
+    // stars_group.position.x -= 1
+    // if (stars_group.position.x < -1000) {
+    //   stars_group.position.x = 1000
     // }
 
     // 摄像机椭圆曲线环绕
-    camera.position.x = 150 * Math.sin(time / 10)
-    camera.position.z = 100 * Math.cos(time / 10)
-    camera.position.y = 90 * Math.cos(time / 10)
+    // camera.position.x = 50 * Math.sin(time / 5)
+    // camera.position.z = 200 * Math.cos(time / 5)
+    // camera.position.y = 90 * Math.cos(time / 5)
+    // 云层飘动
+    // atmosphere.rotation.y = Math.cos(time / 50)
+    // atmosphere.rotation.x = Math.cos(time / 50)
+    // atmosphere.rotation.z = Math.cos(time / 50)
+
 
 
 
